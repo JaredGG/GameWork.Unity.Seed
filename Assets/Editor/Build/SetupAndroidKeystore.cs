16f663c974a4312396d364367acb96a9f85929b2
Jared <jared@playgen.com>
2016-10-13 17:54:55 +0100

    fixed gitignore

---
+++ Assets/Editor/Build/SetupAndroidKeystore.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using GameWork.Unity.Editor.Build;
+using UnityEditor;
+using UnityEngine;
+using EventType = GameWork.Unity.Editor.Build.EventType;
+
+namespace Editor.Build
+{
+    public static class SetupAndrodKeystore
+    {
+        [MenuItem("Tools/Build/Android/Setup Keystore")]
+        [BuildEvent(EventType.Pre)]
+        public static void Setup()
+        {
+            PlayerSettings.Android.keystoreName = Directory.GetParent(Application.dataPath).FullName +
+                                                  "/BuildAssets/Android/seed.keystore";
+            PlayerSettings.Android.keystorePass = "p4$Sw0rd";
+
+            PlayerSettings.Android.keyaliasName = "seed";
+            PlayerSettings.Android.keyaliasPass = "p4$Sw0rd";
+        }
+    }
+}
